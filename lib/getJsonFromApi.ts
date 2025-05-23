09c59bf8da809be9445f34bdd357c2813d95cb6d
Mykola Zakliuka <nicolaizakluka@gmail.com>
2025-05-23 20:16:58 +0300

    build fix

--- lib/getJsonFromApi.ts
+++ lib/getJsonFromApi.ts
@@ -3,12 +3,14 @@ import { DOBA } from '@/lib/constants'
 export const getJsonFromApi = async <T>(
   file: 'timeline' | 'projects' | 'skills'
 ): Promise<T> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SITE_URL}/api/get-data?file=${file}`,
-    {
-      next: { revalidate: DOBA },
-    }
-  )
+  const baseUrl =
+    process.env.NEXT_PUBLIC_SITE_URL ||
+    (process.env.VERCEL_URL && `https://${process.env.VERCEL_URL}`) ||
+    'http://localhost:3000'
+  console.log(typeof window !== 'undefined' && window.location.origin)
+  const res = await fetch(`${baseUrl}/api/get-data?file=${file}`, {
+    next: { revalidate: DOBA },
+  })
 
   if (!res.ok) {
     throw new Error('Failed to fetch JSON from API')
