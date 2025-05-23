dde35f8821c1e0386663220104a1b193a7a60508
Mykola Zakliuka <nicolaizakluka@gmail.com>
2025-05-23 21:26:42 +0300

    build fix

--- lib/getJsonFromApi.ts
+++ lib/getJsonFromApi.ts
@@ -9,10 +9,11 @@ export const getJsonFromApi = async <T>(
     'http://localhost:3000'
   console.log(process.env.VERCEL_URL, 'vu')
   console.log(baseUrl, 'bu')
+  console.log(process.env.NEXT_PUBLIC_VERCEL_URL, 'npvu')
   const res = await fetch(`${baseUrl}/api/get-data?file=${file}`, {
     next: { revalidate: DOBA },
   })
-
+  console.log(res, 'res')
   if (!res.ok) {
     throw new Error('Failed to fetch JSON from API')
   }
