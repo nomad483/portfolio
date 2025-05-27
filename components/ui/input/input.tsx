0735771b388518ba95bdf9a110c2eff46607039b
Mykola Zakliuka <nicolaizakluka@gmail.com>
2025-05-27 19:57:58 +0300

    Fix colors, fixed github actions, added favicons and site.webmanifest

--- components/ui/input/input.tsx
+++ components/ui/input/input.tsx
@@ -8,7 +8,7 @@ const Input = forwardRef<HTMLInputElement, ComponentProps<'input'>>(
       <input
         type={type}
         className={cn(
-          'border-foreground-20 file:text-foreground placeholder:text-muted-foreground focus-visible:ring-foreground-40 flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium autofill:bg-transparent focus-visible:ring-1 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+          'border-foreground-20 file:text-foreground placeholder:text-foreground-50 focus-visible:ring-foreground-40 flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium autofill:bg-transparent focus-visible:ring-1 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
           className
         )}
         ref={ref}
